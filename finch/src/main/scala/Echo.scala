c8f8596a4bb79938b03254109a5b5835686bfc48
Tim Steinbach <tim@nequissimus.com>
2016-08-27 03:05:39 +0000

    Charset, parameter

--- finch/src/main/scala/Echo.scala
+++ finch/src/main/scala/Echo.scala
@@ -3,10 +3,11 @@ import io.finch.circe._
 import com.twitter.finagle.Http
 import com.twitter.util.Await
 import io.circe.generic.auto._
+import java.nio.charset.StandardCharsets
 
-case class Hello(val world: String)
+case class Echo(val param: String)
 
 object Echo extends App {
-  val api = get("hello") { Ok(Hello("World")) }
+  val api = get("echo" :: string) { (s: String) => Ok(Echo(s)) } withCharset StandardCharsets.UTF_8
   Await.ready(Http.server.serve(":8080", api.toServiceAs[Application.Json]))
 }
